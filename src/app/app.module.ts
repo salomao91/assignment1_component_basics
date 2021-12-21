81a73622e9caed2c1484cc5dc9f5afb16ed76487
SalomaoFerreira <salomao1991@gmail.com>
2021-12-21 18:33:09 -0500

    Create WarningAlert Component manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'warning-alert',
+    templateUrl: './warning.component.html'
+})
+
+export class WarningComponent {
+
+}

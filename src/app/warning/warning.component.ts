2f401bcc346d7022019871d753d90a67cd7eb935
SalomaoFerreira <salomao1991@gmail.com>
2021-12-21 19:13:43 -0500

    Output a component content inside another and add CSS style

--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component ({
     selector: 'warning-alert',
-    templateUrl: './warning.component.html'
+    templateUrl: './warning.component.html',
+    styleUrls: ['./warning.component.css']
 })
 
 export class WarningComponent {
